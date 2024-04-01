12b13f57a689df813cd51f6fc5e775084906641e
JUSTINE IMASIKU <justineimasiku21@gmail.com>
2024-04-01 00:24:24 +0000

    Initilize app, create auth pages and onboarding

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
+import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Signup from './Pages/Signup/Signup';
+import Signin from './Pages/Signin/Signin';
+import OnboardingOne from './Pages/first_onboard/OnboardingOne';
+import OnboardingTwo from './Pages/second_onboard/OnboardingTwo';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<OnboardingOne/>} />
+        <Route path="/login" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/onboarding-one" element={<OnboardingOne />} />
+        <Route path="/onboarding-two" element={<OnboardingTwo />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/Pages/second_onboard/OnboardingTwo.jsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import onboardTwo from '../../assets/onboardtwo.png'; 
+import logo from '../../assets/logo.png';
+import { FaArrowRight } from 'react-icons/fa';
+import { useNavigate } from 'react-router-dom';
+import avarta from '../../assets/onboardavarta.png';
+
+const OnboardingTwo = () => {
+    const navigate = useNavigate()
+
+    const audience = [
+        {id: 1, value: 'My team at work', selected: false},
+        {id: 2, value: 'Superior(s) at work', selected: true},
+        {id: 3, value: 'My classmates in school', selected: false},
+        {id: 4, value: 'Customers or clients', selected: false},
+        {id: 5, value: 'groups of 50+ people', selected: false},
+        {id: 6, value: 'Other audience', selected: true},
+    ];
+
+    const savePublicSpeakingAreas = () => {
+        const selectedAreas = audience.filter(area => area.selected)
+        console.log(selectedAreas)
+        navigate('/onboarding-two')
+    }
+  return (
+    <div className='onboard'>
+        <div className="first-col">
+            <div className="logo">
+                <img src={logo} alt="logo" />
+            </div>
+            <div className="flex-column2 full-width">
+            <div className="flex-row">
+                <div className="active-blue twenty-grey"></div>
+                <div className="twenty-grey active-blue"></div>
+            </div>
+            <p>2 of 2</p>
+            </div>
+
+            <div className="flex-column2">
+                <h2>Who is your main audience when giving presentations?</h2>
+            </div>
+            <div className="flex-column3">
+                {
+                    audience.map((area, index) => (
+                        <div key={area.id} className={`flex-row-space audience ${area.selected? "greybd" : "light-blue-bd"}`}>
+                            <div className="flex-row"></div>
+                        </div>
+                    ))
+                }
+            </div>
+            <div className="flex-row-end">
+                <button onClick={() => {savePublicSpeakingAreas()}}>Continue <FaArrowRight/></button>
+            </div>
+        </div>
+        <div className="second-col">
+            <img src={onboardTwo} alt="onboardOne" />
+        </div>
+    </div>
+  )
+}
+
+export default OnboardingTwo
